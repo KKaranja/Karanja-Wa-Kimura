a350127ec4da48d4a0f9196be67a82ddd330693f
isaac karanja <isaackimura@gmail.com>
2020-04-30 23:53:41 +0300

    Updates

---
+++ precache-manifest.b7953ec2632b46e3c4c0261e2357f094.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ca998f3b9d4e37a5c4f4bff6aa9499",
+    "url": "/Karanja-Wa-Kimura/index.html"
+  },
+  {
+    "revision": "945be097a54d867822fa",
+    "url": "/Karanja-Wa-Kimura/static/css/main.0a0b792b.chunk.css"
+  },
+  {
+    "revision": "c07277c012357113b3c3",
+    "url": "/Karanja-Wa-Kimura/static/js/2.16f55660.chunk.js"
+  },
+  {
+    "revision": "0a8c051bdf17e231ea249c5452d1df44",
+    "url": "/Karanja-Wa-Kimura/static/js/2.16f55660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945be097a54d867822fa",
+    "url": "/Karanja-Wa-Kimura/static/js/main.d8c74f29.chunk.js"
+  },
+  {
+    "revision": "1e3e52d6be2b793d0bca",
+    "url": "/Karanja-Wa-Kimura/static/js/runtime-main.324ebddf.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/Karanja-Wa-Kimura/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/Karanja-Wa-Kimura/static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
