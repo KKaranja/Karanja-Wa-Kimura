a66e8be3a88745dfa2d8e10919cdbb3110976815
isaac karanja <isaackimura@gmail.com>
2020-05-01 00:59:42 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-// import { Route } from "react-router-dom";
-import { BrowserRouter as Router, Route, } from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
-import CssBaseline from '@material-ui/core/CssBaseline';
-
-import Home from "./components/index"
-import Resume from "./components/Resume";
-import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
-    <Router>
-      <CssBaseline>
-        <Route exact path="/" component={Home} />
-        <Route path="/resume" component={Resume} />
-        <Route path="/portfolio" component={Portfolio} />
-      </CssBaseline>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
