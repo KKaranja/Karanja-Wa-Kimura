6c5795eeaef05ef9c5469abf388efb8c6638a430
isaac karanja <isaackimura@gmail.com>
2020-05-02 14:56:44 +0300

    deployment rule changed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router >
+  <Router  >
     <App />
   </Router>,
   document.getElementById("root")
